63845fcb9d9f726b5fbff2abd8fdc2150893da7e
Frederick Tetteh <frederick.tetteh@ontariotechu.net>
2023-02-01 19:25:17 -0500

    Added a search for people section with radio buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import { searchForShows } from '../API/tvmaze';
 import { useState , React} from 'react'
 import { MainPageLayout } from '../components/MainPageLayout'
-
+import { searchForPeople } from '../API/tvmaze';
 // const pages = {
 //   home: 'HomePage',
 //   starred: 'Starred',
@@ -14,27 +14,39 @@ export const Home = function ()  {
 //we are going to use the target to 
   // were are checking if the state has been updated
   const[apiData,setapiData] = useState(null);
-
+  const[searchOption, setSearchOption] = useState('shows')//set the default state to the input shows
   const[apiDataError, setapiDataError] = useState(null);
-  console.log(apiDataError);
+  console.log(searchOption);
   const onSearchChange = (ev) =>{//takes the parameter the event object //we are going to use the target to 
 
     setsearchstr(ev.target.value);//we are now getting the actual input typed in the box we have updated the state of the value now
   
   };
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
  
-   const onSearch = async (ev) => {
+  const onSearch = async (ev) => {
      ev.preventDefault();
     
     try{
-    setapiDataError(null);
-     const result = await searchForShows(searchstr);
-     setapiData(result);
+      setapiDataError(null);
+    
+      if (searchOption === 'shows'){
+        const result = await searchForShows(searchstr);
+        setapiData(result);
+      }
+      else{
+        const result = await searchForPeople(searchstr);
+        setapiData(result);  
+      }
+  
+    
     }
     catch (error){
       setapiDataError(error);
       //uses the searchForshows from the tvmaze in ordder to check for an error once it does throws an erro which is caught and set to apiDataError
-    }
+    };
   
     //Event interference that tells the user if the event isnt handled so event is given its default action should not be done
     // in this case instead of defaulting submitting the form to nothing instead it will just do nothing when the button is pressed
@@ -48,13 +60,15 @@ export const Home = function ()  {
       }
 
       if (apiData){
-        return apiData.map((data) => (
+        return apiData[0].show ? apiData.map((data) => (
         <div key= {data.show.id} >{data.show.name}</div>
-        ))
+        )) :  apiData.map((data) => (
+          <div key= {data.person.id} >{data.person.name}</div>
+          ));
 
       }
       return null;
-  }
+  };
   //Introduce one way data binding only the the state is updated console "Frederick" but the input text box was not updated
   //Two way binding we update the text as well as the input
   return (
@@ -66,6 +80,16 @@ export const Home = function ()  {
     <MainPageLayout/>
     <form onSubmit={onSearch}>
     <input type = "text" onChange={onSearchChange} value={searchstr}/>
+
+    <label>
+      Shows
+      <input type='radio' name='search-option' value='shows' checked={searchOption === 'shows'} onChange={onRadioChange}/>
+    </label>
+    <label>
+      Actors
+      <input type='radio' name='search-option' value='actors' checked={searchOption ==='actors'} onChange={onRadioChange}/>
+    </label>
+
     <button type='submit'>Search</button>
     </form>
     <div>
