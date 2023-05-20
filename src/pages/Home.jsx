b6e5b88ad0b3a6d2605d4cc6ec11134c0d8d483c
Frederick Tetteh <frederick.tetteh@ontariotechu.net>
2023-05-20 13:06:08 -0400

    Implmented usequery in the Home page and fixed the undefined issue when fetching data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable */ 
 import { searchForShows } from '../API/tvmaze';
+import {useQuery} from '@tanstack/react-query';
 import { useState , React} from 'react'
 import { MainPageLayout } from '../components/MainPageLayout'
 import { searchForPeople } from '../API/tvmaze';
 import { SearchForm } from '../components/SearchForm';
 import { Show_Grid } from '../components/shows/Show_Grid';
 import { Actor_Grid } from '../components/actors/Actor_Grid';
+
 // const pages = {
 //   home: 'HomePage',
 //   starred: 'Starred',
@@ -14,39 +16,25 @@ import { Actor_Grid } from '../components/actors/Actor_Grid';
 export const Home = function ()  {
   // const {home, starred} = pages
   
+  const [filter, setFilter] = useState(null);
 
-  const[apiData,setapiData] = useState(null);
-  const[apiDataError, setapiDataError] = useState(null);
-  
-  
-  
- 
+  const { data:apiData, error:apiDataError } = useQuery({
+      queryKey: ['search', filter],
+      queryFn: () => 
+      filter.searchOption === 'shows'
+        ? searchForShows(filter.searchstr)
+        : searchForPeople(filter.searchstr),
+      //filter changes when ever we press the search button
+      enabled: !!filter,//;let react query know when we want to run and not run the fetch function
+      refetchOnWindowFocus: false,
+      
+    });
+    console.log(filter)
   const onSearch = async ({searchstr, searchOption}) => {//we passed in the options and destructured it 
     
-    
-    try{
-      setapiDataError(null);
-      let result;  
-    
-      if (searchOption === 'shows'){
-        result = await searchForShows(searchstr);
-        setapiData(result);
-      }
-      else{
-        result = await searchForPeople(searchstr);
-        setapiData(result);  
-      }
-  
-    
-    }
-    catch (error){
-      setapiDataError(error);
-      //uses the searchForshows from the tvmaze in ordder to check for an error once it does throws an erro which is caught and set to apiDataError
-    };
-  
-    //Event interference that tells the user if the event isnt handled so event is given its default action should not be done
-    // in this case instead of defaulting submitting the form to nothing instead it will just do nothing when the button is pressed
-};
+    setFilter({searchstr , searchOption});
+};//Event interference that tells the user if the event isnt handled so event is given its default action should not be done
+// in this case instead of defaulting submitting the form to nothing instead it will just do nothing when the button is pressed
 
   const renderapiData = () => {
       if (apiDataError){
